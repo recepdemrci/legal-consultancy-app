f24d80c5a2f723ba936d3d4fb9a5e0490a1569e5
recepdemrci <recepdemiric.14@gmail.com>
2022-11-21 17:50:30 +0000

    NavToggler was added

---
+++ src/components/NavbarToggler.jsx
@@ -0,0 +1,11 @@
+import {React} from 'react';
+
+function NavbarToggler() {
+  return (
+    <button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  );
+}
+
+export default NavbarToggler;
