8eeeb80694f0ca6a1ac9c3e786c2de7428b59e4b
recepdemrci <recepdemiric.14@gmail.com>
2022-11-21 18:10:59 +0000

    NavbarList and NavbarItem were added

---
+++ src/components/NavbarList.jsx
@@ -0,0 +1,24 @@
+import {React} from 'react';
+import NavbarItem from './NavbarItem';
+
+const navbarItems = [
+  {href: '#home-container', item: 'ANASAYFA'},
+  {href: '#home-container', item: 'HİZMETLERİMİZ'},
+  {href: '#home-container', item: 'HAKKIMIZDA'},
+  {href: '#home-container', item: 'KADROMUZ'},
+  {href: '#home-container', item: 'İLETİŞİM'}
+]
+
+function NavbarList() {
+  return (
+    <div class="collapse navbar-collapse text-center" id="navbarColor03">
+      <ul class="navbar-nav mx-auto my-2">
+        {navbarItems.map(navbarItem => (
+          <NavbarItem href={navbarItem.href} item={navbarItem.item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default NavbarList;
