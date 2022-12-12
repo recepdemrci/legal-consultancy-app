be9a78aa4b6c78e6034937302e54ac1382748543
recepdemrci <recepdemiric.14@gmail.com>
2022-12-12 18:46:14 +0000

    format file

--- src/components/BottomContainer.jsx
+++ src/components/BottomContainer.jsx
@@ -9,20 +9,40 @@ function BottomContainer() {
     <div class="bottom-container" id="bottom-container">
       <div class="row justify-content-center">
         <div class="col-xl-4 d-none d-lg-block text-end align-self-center">
-          <Card 
+          <Card
             name="experience-card"
-            title={<p>FİKRİ VE SANAT MÜLKİYET<br/>İŞ VE SOSYAL GÜVENLİK<br/>İCRA VE İFLAS<br/>TİCARET HUKUKU</p>}
+            title={
+              <p>
+                FİKRİ VE SANAT MÜLKİYET
+                <br />
+                İŞ VE SOSYAL GÜVENLİK
+                <br />
+                İCRA VE İFLAS
+                <br />
+                TİCARET HUKUKU
+              </p>
+            }
           />
         </div>
         <div class="col-xl-4 col-md-6 col-8 text-center">
           <Avatar image={lawyerImg} />
         </div>
         <div class="col-xl-4 col-md-6 col-8 text-center align-self-center">
-          <Card 
+          <Card
             name="cv-card"
             title="NURETTİN SELÇUK ÖZTÜRK"
             body={
-              <p><br/>AVUKAT<br/>İSTANBUL<br/><br/>+90 532 660 02 85<br/>nurettinselcukozturk@gmail.com</p>
+              <p>
+                <br />
+                AVUKAT
+                <br />
+                İSTANBUL
+                <br />
+                <br />
+                +90 532 660 02 85
+                <br />
+                nurettinselcukozturk@gmail.com
+              </p>
             }
           />
         </div>
