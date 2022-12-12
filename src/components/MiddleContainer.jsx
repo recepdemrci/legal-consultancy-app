49631438eb78877f129a6e3d0b2656d55a1a5707
recepdemrci <recepdemiric.14@gmail.com>
2022-12-12 17:17:47 +0000

    format middle-container file

--- src/components/MiddleContainer.jsx
+++ src/components/MiddleContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "assets/styles/MiddleContainer.css"
+import "assets/styles/MiddleContainer.css";
 import containerImg from "assets/images/middle-container.png";
 import Card from "components/Card";
 import TextBackground from "components/TextBackgroud";
@@ -10,42 +10,55 @@ function MiddleContainer() {
     <div class="middle-container" id="middle-container">
       <div class="row justify-content-between row-top">
         <div class="col-xl-5 col-lg-6 col-9 mb-lg-0 mb-5">
-          <Card 
+          <Card
             name="info-card"
             title="HAKKIMIZDA"
             line={true}
             body={
-              <p>Av. Nureddin Selçuk Öztürk tarafından kurulan, yerli ve yabancı müvekkillerine özellikle şu alanlarda danışmanlık vermekteyiz;<br/>
-                <br/>
-                -  Fikri/Sanai mülkiyet hukuk<br/>
-                -  İş ve sosyal güvenlik hukuk<br/>
-                -  İcra ve iflas hukuku<br/>
-                <br/>
-                Ayrıca müvekkillerinin karşılaştığı uyuşmazlıkların, hukuk zemininde çözüm ve yöntemlerinin belirlenmesi,
-                önleyici avukatlık ve yargı süreçlerinin tümünün yürütülmesi hizmetlerinin özveri ile sağlanması gayreti içerisindeyiz.
+              <p>
+                Av. Nureddin Selçuk Öztürk tarafından kurulan, yerli ve yabancı
+                müvekkillerine özellikle şu alanlarda danışmanlık vermekteyiz;
+                <br />
+                <br />
+                - Fikri/Sanai mülkiyet hukuk
+                <br />
+                - İş ve sosyal güvenlik hukuk
+                <br />
+                - İcra ve iflas hukuku
+                <br />
+                <br />
+                Ayrıca müvekkillerinin karşılaştığı uyuşmazlıkların, hukuk
+                zemininde çözüm ve yöntemlerinin belirlenmesi, önleyici
+                avukatlık ve yargı süreçlerinin tümünün yürütülmesi
+                hizmetlerinin özveri ile sağlanması gayreti içerisindeyiz.
               </p>
             }
           />
         </div>
         <div class="col-lg-6 col-12">
           <div class="col-xl-8 col-lg-10 col-8 text-center offset-4">
-            <Card 
+            <Card
               name="slogan-card"
               title="HAK, HUKUKEN KORUNAN MENFAAT VE HELAL KAVRAMLARINI ÖZEL İLGİ ODAĞIMIZDIR."
             />
           </div>
           <div class="col-xl-8 col-lg-10 col-8 align-self-start">
-            <img src={containerImg} alt="middle-container-img" class="img-fluid mb-lg-0 mb-5 direction-img" />
+            <img
+              src={containerImg}
+              alt="middle-container-img"
+              class="img-fluid mb-lg-0 mb-5 direction-img"
+            />
           </div>
         </div>
       </div>
       <div class="row row-bottom">
-        <TextBackground col='col-xl-5 col-lg-6' text="ÖNLEYİCİ AVUKATLIK" textAlign='text-start'/>
+        <TextBackground
+          col="col-xl-5 col-lg-6"
+          text="ÖNLEYİCİ AVUKATLIK"
+          textAlign="text-start"
+        />
         <div class="col-xl-4 col-lg-6 col-7 text-center offset-lg-0 mb-lg-0 mb-5 offset-5">
-          <Card 
-            name="word-card"
-            title="HESAP VEREBİLİRLİK    ŞEFFAFLIK"
-          />
+          <Card name="word-card" title="HESAP VEREBİLİRLİK    ŞEFFAFLIK" />
         </div>
       </div>
       <div class="row">
